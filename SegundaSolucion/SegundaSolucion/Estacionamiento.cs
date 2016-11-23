570bbda345bcd89ac4cdde336b61bc9e18b025d8
federicoledo <federicoledo13@gmail.com>
2016-11-22 23:48:26 -0300

    Add files via upload

---
+++ SegundaSolucion/SegundaSolucion/Estacionamiento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testeando
+{
+    public class Estacionamiento
+    {
+        public List<Vehiculo> listaVehiculos;
+
+        public Estacionamiento()
+        {
+            this.listaVehiculos = new List<Vehiculo>();
+        }
+    }
+}
---
+++ SegundaSolucion/Testeandoo/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testeando;
+
+
+namespace Testeandoo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]   
+        public void InstanciaEstacionamiento()
+        {
+            Estacionamiento est1 = new Estacionamiento();
+            Assert.IsNotNull(est1);            
+        }
+        
+
+        public void ListadoInstanciado()
+        {
+            Estacionamiento est1 = new Estacionamiento();
+            Assert.IsNotNull(est1.listaVehiculos);
+        }
+    }
+}
